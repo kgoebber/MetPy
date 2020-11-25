53c352c1020d59ef88239ae06de99f1e8d083c9c
kgoebber <kgoebber@users.noreply.github.com>
2020-11-25 22:29:53 +0000

    Deploy dev to GitHub Pages

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.post578+gfcddc28',
+    VERSION: '0.0.post616+ge63b136',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
